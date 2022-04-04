2bfcd3c9a19a59f1a3190d0a3f1c0f5235ce1a71
sagordevnath <ssagor64@gmail.com>
2022-04-04 07:53:07 +0600

    set route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import About from "./components/About/About";
-import Blogs from "./components/Blogs/Blogs";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import NotFound from "./components/NotFound/NotFound";
+import Home from "./components/Home/Home";
 import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,20 +8,25 @@ const Home = () => {
   return (
     <>
       <div className="home-container">
-        <div>
+        <div className="details-container">
           <h1>Home</h1>
         </div>
-        <div>
+        <div className="image-container">
           <img
             src="http://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png"
             alt=""
           />
         </div>
       </div>
-      <div>
-        {reviews.map((review) => (
-          <p>{review.name}</p>
-        ))}
+      <div className="review-container">
+        <div className="review-heading">
+          <h1>Language Reviews (3)</h1>
+        </div>
+        <div className="review-list">
+          {reviews.map((review) => (
+            <p>{review.name}</p>
+          ))}
+        </div>
       </div>
     </>
   );
